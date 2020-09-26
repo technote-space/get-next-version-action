519df4b494775510cf8c32691a71553921f47885
Technote <technote.space@gmail.com>
2020-09-26 16:13:20 +0900

    feat: use log helper

--- src/process.ts
+++ src/process.ts
@@ -1,5 +1,6 @@
 import {Context} from '@actions/github/lib/context';
-import {Logger, Utils, ApiHelper} from '@technote-space/github-action-helper';
+import {Utils, ApiHelper} from '@technote-space/github-action-helper';
+import {Logger} from '@technote-space/github-action-log-helper';
 import {exportVariable, getInput, setOutput} from '@actions/core' ;
 import {getCurrentVersion, getNextVersion} from './utils/version';
 
